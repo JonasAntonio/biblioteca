16a334fc10e0610ce70a8b7df00fc67e656f6564
JonasAntonio <jayv.jonas@gmail.com>
2019-06-03 17:57:38 -0300

    Bkp banco

---
+++ db/bkp/03-06/bibliotecaLPAW_tb_livro.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 5.7.26, for Linux (x86_64)
+--
+-- Host: localhost    Database: bibliotecaLPAW
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.36-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_livro`
+--
+
+DROP TABLE IF EXISTS `tb_livro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_livro` (
+  `idtb_livro` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `isbn` varchar(255) NOT NULL,
+  `edicao` varchar(4) DEFAULT NULL,
+  `ano` year(4) NOT NULL,
+  `upload` varchar(255) DEFAULT NULL,
+  `tb_editora_id_tb_editora` int(11) NOT NULL,
+  `tb_categoria_id_tb_categoria` int(11) NOT NULL,
+  PRIMARY KEY (`idtb_livro`),
+  UNIQUE KEY `isbn_UNIQUE` (`isbn`),
+  KEY `fk_tb_livro_tb_editora1_idx` (`tb_editora_id_tb_editora`),
+  KEY `fk_tb_livro_tb_categoria1_idx` (`tb_categoria_id_tb_categoria`),
+  CONSTRAINT `fk_tb_livro_tb_categoria1` FOREIGN KEY (`tb_categoria_id_tb_categoria`) REFERENCES `tb_categoria` (`idtb_categoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tb_livro_tb_editora1` FOREIGN KEY (`tb_editora_id_tb_editora`) REFERENCES `tb_editora` (`idtb_editora`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_livro`
+--
+
+LOCK TABLES `tb_livro` WRITE;
+/*!40000 ALTER TABLE `tb_livro` DISABLE KEYS */;
+INSERT INTO `tb_livro` VALUES (1,'Livro 1','123456','1',0000,'',1,1),(6,'livro 3','123','123',2019,NULL,1,1),(7,'livro 5','321','123',2019,NULL,1,1),(9,'asdasd','12345678945465','3213',2019,NULL,1,1),(10,'teste','3545612315','1',2019,NULL,1,1),(12,'teste','1235463213545449684651','1',2019,NULL,1,1),(14,'teste','1321354684651354684351387684','1321',2019,NULL,1,1),(16,'teste','321315461215432','12',2019,NULL,1,1),(17,'teste','21321313131311321','23',2019,NULL,1,1),(23,'teste','3213132132132313212','1',2019,NULL,1,1),(25,'Teste','12345678998765432112','1',2019,NULL,1,1),(28,'Teste','123456789987654321121','1',2019,NULL,1,1),(31,'Teste','12345678998765432112321','1',2019,NULL,1,1),(32,'teste','32131354351431','11',2019,NULL,1,1),(35,'Teste','12345678998765432','1',2019,NULL,1,1),(37,'Teste','1234567899876','1',2019,NULL,1,1),(40,'Teste','12345678998765432112123123123123123','1',2019,NULL,1,1),(43,'Teste','123456789987654321121111111111','1',2019,NULL,1,1),(46,'Teste','123456789312312312','1',2019,NULL,1,1),(52,'Teste','123456789987654321123222222222232','1',2019,NULL,1,1),(53,'Teste','0000012345678998765432112','1',2019,NULL,1,1),(54,'Teste','12345600000','1',2019,NULL,1,1),(56,'Teste','123456789987000000421','1',2019,NULL,1,1),(59,'Teste','45678998765432112','1',2019,NULL,1,1),(60,'Teste','123321123321123321','1',2019,NULL,1,1);
+/*!40000 ALTER TABLE `tb_livro` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-06-03 17:57:17
